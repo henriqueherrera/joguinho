d583fa8872fc5bf39a0441b56535717493b9629e
mirasia <henriqueherrera9@gmail.com>
2019-03-24 14:00:43 -0300

    versao atualizada

--- Jogo_version_1.3.cpp
+++ Jogo_version_1.3.cpp
@@ -4,57 +4,114 @@
 #include "sprite.h"
 #include <vector>
 #include <string.h>
+#include <SDL2/SDL_ttf.h>
+
 using namespace std;
+
 SDL_Window *window;
 SDL_Renderer *renderer, *renderer1;
 SDL_Event Event;
  
+TTF_Font *font;
+
+int style;
 int SCREEN_WIDTH = 1000;
 int SCREEN_HEIGHT = 750;
-int mainTimer,run = 0;
-bool cima = false, baixo = false, esquerda = false, direita = false, create_run = false;
-char WINDOW_NAME[32] = "JOGO";
-sprite background[3];
+int mainTimer;
 
+bool cima = false, baixo = false, esquerda = false, direita = false, create_run = false;
+
+char WINDOW_NAME[32] = "JOGO";
+
+sprite background[3],
+       run[1]
+       //walk[],
+       //dead[],
+;
+
+/*void txt_screen()
+{
+	if(TTF_Init() == -1)//if
+	{
+		cout<<"TTF_Init" << TTF_GetError()<<endl;
+	}
+	else
+	{
+		style = TTF_GetFontStyle(font);
+	       	cout <<"The fonte style is:"<<endl;
+		if( style == TTF_STYLE_NORMAL)
+		{
+			cout<< "normal" << endl;
+		}	
+	}
+}*/
 void back()
 {
-	if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
-		std::cout << SDL_GetError() << std::endl;
-        } else {
+	if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		cout << SDL_GetError() << endl;
+        } else 
+	{
                 window = SDL_CreateWindow(WINDOW_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
-                if(window == NULL){
+                if(window == NULL)
+		{
                         cout << SDL_GetError() << endl;
-                } else {
+                }
+	       	else 
+		{
                         renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-			if(renderer == NULL){
-                                std::cout << SDL_GetError() << std::endl;
-                        } else {
+			if(renderer == NULL)
+			{
+                                cout << SDL_GetError() << endl;
+                        } else 
+			{
                                 SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
                                 int IMG_FLAGS = IMG_INIT_PNG;
-                                if(!(IMG_Init(IMG_FLAGS) & IMG_FLAGS)){
+                                if(!(IMG_Init(IMG_FLAGS) & IMG_FLAGS))
+				{
                                         std::cout << IMG_GetError() << std::endl;
-                                } else {
+                                }
+			       	else 
+				{
+					run[0].texture = IMG_LoadTexture(renderer, "./run.png");
+                                       	SDL_QueryTexture(run[0].texture, NULL, NULL, &run[0].w, &run[0].h);
+                                        run[0].y = 300;
+					run[0].x = 300; 
+					run[0].rect.w = 50;
+					run[0].rect.h = 50;
+					run[0].rect.x = run[0].x;
+					run[0].rect.y = run[0].y;
+                               
 					for(int i = 0; i < 3; i++)
 					{
-                                        	background[i].texture = IMG_LoadTexture(renderer, "./BG.png");
-                                       	 	SDL_QueryTexture(background[i].texture, NULL, NULL, &background[i].w, &background[i].h);
-                                        	background[i].x = 1000*i;
-                                        	background[i].y = 0;
+                                        	if  ( (i == 0) || ( i == 2) )
+						{
+							background[i].texture = IMG_LoadTexture(renderer, "./fundo1.png");
+                                       	 		SDL_QueryTexture(background[i].texture, NULL, NULL, &background[i].w, &background[i].h);
+                                        		background[i].x = 1280*i;
+						}
+						if (i == 1)
+						{
+							background[i].texture = IMG_LoadTexture(renderer, "./fundo2.png");
+                                       	 		SDL_QueryTexture(background[i].texture, NULL, NULL, &background[i].w, &background[i].h);
+                                        		background[i].x = 1280*i;
+						}
+							
+						background[i].y = 0;
                                         	background[i].rect.w = background[i].w;
                                         	background[i].rect.h = background[i].h;
                                         	background[i].rect.x = background[i].x;
                                         	background[i].rect.y = background[i].y;
-                                        	background[i].velocity = 20;
+                                        //	background[i].velocity = 20;
+						
 					}
-	
-
-                                }
+			 }
                         }
                 }
         }
 
 }
-//função que move o fundo da tela
+//função que move o fundo da telavoid movBackground()
 void movBackground()
 {
 	for(int i  = 0; i<3; i++)
@@ -64,39 +121,50 @@ void movBackground()
 		{
 		
 			background[0].rect.x = 0 ;
-			background[1].rect.x = 1000;
-			background[2].rect.x = 2000;
+			background[1].rect.x = 1280;
+			background[2].rect.x  = 2560;
 		}
 	}
  
 }
 //gera as imagens
-blit(void)
+void blit(void)
 {
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, background[0].texture, NULL, &background[0].rect);
     SDL_RenderCopy(renderer, background[1].texture, NULL, &background[1].rect);
     SDL_RenderCopy(renderer, background[2].texture, NULL, &background[2].rect);
-	
+    SDL_RenderCopy(renderer, run[0].texture, NULL, &run[0].rect);
     SDL_RenderPresent(renderer);
 
 }
-void tique()
+void controle()
 {
-        movBackground();
-        blit();
-        
 }
 
+//roda as funções principais
+
+void tique()
+{
+	//txt_screen();
+	blit();
+	
+	movBackground();
+        
+
+
+        
+}
 
 int main(int argc, char *argv[])
 {
 
         back();       
  	
-        mainTimer = SDL_GetTicks();
+        int mainTimer = SDL_GetTicks();
         bool close = false;
-        while(!close)
+        
+	while(!close)
         {
                 while(SDL_PollEvent(&Event) != 0)
                 {
@@ -144,9 +212,7 @@ int main(int argc, char *argv[])
                                         break;
                                 }
                         }
-                }
- 
-                /*if(mainTimer + 10 < SDL_GetTicks()){
+               /*if(mainTimer + 10 < SDL_GetTicks()){
                         if(cima){
                                 player.rect.y -= player.velocity;
                         }
@@ -161,15 +227,15 @@ int main(int argc, char *argv[])
                         }
                         mainTimer = SDL_GetTicks();
                 }*/
-		if(mainTimer +5 < SDL_GetTicks())
-		{
-			tique();//gera a parte grafica			
-			mainTimer = SDL_GetTicks();
-			
+                }
+	
+	if(mainTimer +5 < SDL_GetTicks())
+	{
+		tique();//gera a parte grafica			
+		mainTimer = SDL_GetTicks();
+	}
+	}
 
-		}
-        }
- 
         SDL_DestroyWindow(window);
         SDL_Quit();
  
